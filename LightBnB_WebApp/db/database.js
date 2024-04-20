2fce3477398880c90bf5df63cde4ef1cc6c95854
Cynthia <cynthia-lam@outlook.com>
2024-04-20 19:20:01 -0400

    Fix price from cents to dollar and fix variable names

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -131,10 +131,10 @@ const getAllProperties = (options, limit = 10) => {
     }
 
     // price
-    if (minimum_price_per_night && maximum_price_per_night) {
-      queryParams.push(`${options.minimum_price_per_night}`);
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night * 100);
       queryString += `WHERE cost_per_night > $${queryParams.length} `;
-      queryParams.push(`${options.maximum_price_per_night}`);
+      queryParams.push(options.maximum_price_per_night * 100);
       queryString += `AND cost_per_night < $${queryParams.length} `;
     }
 
